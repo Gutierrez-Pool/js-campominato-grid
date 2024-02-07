385e9bb369afb9b5f010d9e581f1224df43f61b1
Pool-Gutierrez <pool.gutierrezv@gmail.com>
2024-02-07 17:59:18 +0100

    aggiunta griglia

--- js/script.js
+++ js/script.js
@@ -5,3 +5,34 @@
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 // Potete realizzare lo stile che volete, sin quando tutto è leggibile con facilità.
+
+
+const gridElement = document.querySelector("#grid");
+
+const startButton = document.querySelector("#start-button");
+
+startButton.addEventListener("click",
+    function() {
+
+        for (let i = 1; i <= 100; i++) {
+        
+            const newElement = document.createElement("div");
+        
+            newElement.innerText = i;
+            newElement.classList.add ("square");
+        
+            
+            gridElement.append(newElement);
+            
+            // Colore
+            newElement.addEventListener ("click", function () {
+                
+                this.classList.toggle ("active");
+                
+                
+                console.log (newElement);
+            })
+        } 
+    }
+    )
+    
